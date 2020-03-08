084b60b194db168e4c5884ec758ae154109437a2
Jamshid93 <hamzayevjf@gmail.com>
2020-03-09 01:51:05 +0500

    Arithmetic methods

--- arithmetic.py
+++ arithmetic.py
@@ -5,3 +5,7 @@ print(10/3)
 print(10//3)# ikki slesh "//" qo'ysak bizga faqat integer ko'rinishini ya'ni butun qismini chiqarib beradi.
 print(10%3)
 print(10**3)# bu darajaga ko'tarish.
+x=12
+x=x+4
+x+=4#bu yerda bu ikkalasi xam bir xil 
+print(x)
