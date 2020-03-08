03482024623e0a9c9a215b4744eb0c494fa9b821
Jamshid93 <hamzayevjf@gmail.com>
2020-03-09 01:24:35 +0500

    find index

---
+++ findindex.py
@@ -0,0 +1,3 @@
+full_name="Khamzaev Jamshid"
+print(full_name.find('a')) # lekin shu yerda xato borda xozir men 'a' harfining indeksini topishni buyursam bu metod faqat ,meng birinchi a harfinikini chiqarib bermoqda. ? Men xamma  'a' larnikini bir vaqtning o'zida qanday topaman?.
+print(full_name.find('m'))
