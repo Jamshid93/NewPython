7b5e8fae81f804af993b9f647853412223392c81
Jamshid93 <hamzayevjf@gmail.com>
2020-03-09 01:32:58 +0500

    replace method

--- replacemethod.py
+++ replacemethod.py
@@ -1,2 +1,2 @@
 course="Life is beautiful"
-print(course.replace('is','amazing and'))
+print(course.replace('is','amazing and'))# bu method orqali biz matndagi kerakli bo'lgan so'zni boshqa bitta so'z bilan almashtiramiz.
