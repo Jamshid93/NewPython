12fb5e29efa795a10e8aca0af3b7efcaf9ff71ba
Jamshid93 <hamzayevjf@gmail.com>
2020-03-09 02:23:09 +0500

    if statement

---
+++ ifstatemant.py
@@ -0,0 +1,13 @@
+is_hot=False
+is_cold=False
+
+if is_hot: # agar bugun issi bo'lsa shunday deb chiqar deyapti 
+    print("It is a hot day")
+    print("Drink plenty of water")
+elif is_cold: # yo'q agar issi bo'lmasa bunday deb chiqar deyapti
+    print("Is is a cold day")
+    print("Wear warm clothes")
+else: # agar na issi va na sovuq bo'lsa umuman boshqa ob xavo bo'lsa bunday deb aytgin deyapti
+    print("It is a lovely day")
+
+print("Enjoy your day !")
--- importmath.py
+++ importmath.py
@@ -1,3 +1,4 @@
 import math
 print(math.ceil(3.8))# bu ceil function xam yaxlitlaydi
 print(math.floor(3.8))# bu floor function butunini qaytaradi.
+
