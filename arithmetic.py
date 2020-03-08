cc1df13d9140e9bdfafe3f9e2812f1cffd20cff9
Jamshid93 <hamzayevjf@gmail.com>
2020-03-09 01:45:21 +0500

    Arithmetic execises

---
+++ arithmetic.py
@@ -0,0 +1,7 @@
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)# ikki slesh "//" qo'ysak bizga faqat integer ko'rinishini ya'ni butun qismini chiqarib beradi.
+print(10%3)
+print(10**3)# bu darajaga ko'tarish.
